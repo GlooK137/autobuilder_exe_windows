6cc3ead1f12297c6fb6f4425dc4d381660c3a29e
GlooK137 <dima2014711@gmail.com>
2022-08-01 23:15:06 +0500

    change script

--- builder.py
+++ builder.py
@@ -6,21 +6,21 @@ async def build(path):
 
 
     script = [
-        [ system, rf"python -m venv {path}\venv"],
-        [ system, rf"{path}\venv\Scripts\pip install -r {path}\requirements.txt"],
-        [ system, rf"{path}\venv\Scripts\pyinstaller -F {path}\main.py"],
-        [ rmtree, rf"{path}\venv"],
-        [ copy2,  rf"{path}\dist\main.exe {path}\main.exe"],
-        [ rmtree, rf"{path}\dist" ],
-        [ rmtree, rf"{path}\build" ],
-        [ remove, rf"{path}\main.spec" ]
+        [ system, [rf"python -m venv {path}\venv"]],
+        [ system, [rf"{path}\venv\Scripts\pip install -r {path}\requirements.txt"]],
+        [ system, [rf"{path}\venv\Scripts\pyinstaller -F {path}\main.py"]],
+        [ rmtree, [rf"{path}\venv"]],
+        [ copy2,  [rf"{path}\dist\main.exe",rf"{path}\main.exe"]],
+        [ rmtree, [rf"{path}\dist"] ],
+        [ rmtree, [rf"{path}\build"] ],
+        [ remove, [rf"{path}\main.spec"] ]
     ]
 
     for command in script:
         
         try:
 
-            command[0](command[1])
+            command[0](*command[1])
 
         except Exception as e:
 
