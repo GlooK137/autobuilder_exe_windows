b5b5229483a43ad087efb1701e52f9363423ed2c
GlooK137 <dima2014711@gmail.com>
2022-08-01 22:41:02 +0500

    working without .bat

--- builder.py
+++ builder.py
@@ -1,6 +1,32 @@
-from os import system
+from os import system, remove
+from shutil import rmtree, copy2
+
+
 async def build(path):
-    system("copy build_exe.bat "+path+"\\build_exe.bat")
-    system(f"cd {path} && build_exe.bat")
+
+
+    script = [
+        [ system, f"cd {path}"],
+        [ system, r"python -m venv venv"],
+        [ system, r"venv\Scripts\pip install -r requirements.txt"],
+        [ system, r"venv\Scripts\pyinstaller -F main.py"],
+        [ rmtree, "venv"],
+        [ copy2,  r"dist\main.exe main.exe"],
+        [ rmtree, "dist" ],
+        [ rmtree, "build" ],
+        [ remove, "main.spec" ]
+    ]
+
+    for command in script:
+        
+        try:
+
+            command[0](command[1])
+
+        except Exception as e:
+
+            print(f"{command[0]}({command[1]})",e)
+            return 0
+
     file = open(path+"/main.exe","rb")
     return file
